1734c1f36a59e8c0dd491b97a749a2f8b95c1615
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-13 19:01:12 +0530

    Working One with @ConfiguranProperties and getters and setters that go with them

--- src/main/java/com/nikhilzzz/SpringBootP03/Beans/Flipkart.java
+++ src/main/java/com/nikhilzzz/SpringBootP03/Beans/Flipkart.java
@@ -3,25 +3,35 @@ package com.nikhilzzz.SpringBootP03.Beans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-//@CofigurationProperties(prefix = "db")
+@ConfigurationProperties(prefix = "db")
 public class Flipkart {
-    Flipkart(){
+
+    Flipkart() {
         System.out.println("Flipkart CONSTRUCTOR");
     }
+
     @Autowired
-   // @Qualifier("jp")
+    // @Qualifier("jp")
     Partners pat;
 
-    @Value("${db.url}")
+    // @Value("${db.url}")
     String URL;
 
-    public void paymentGateway(String Amount)
-    {
-
+    public void paymentGateway(String Amount) {
         System.out.println(URL);
         pat.payments(Amount);
+    }
+
+    // ✅ Required getter and setter for Spring to bind property
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
     }
 }
