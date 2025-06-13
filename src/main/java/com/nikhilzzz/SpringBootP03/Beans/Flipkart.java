e103575c02c4f1417823e3e898faf5c171aa1bfc
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-13 18:56:39 +0530

    Working One with @Value

--- src/main/java/com/nikhilzzz/SpringBootP03/Beans/Flipkart.java
+++ src/main/java/com/nikhilzzz/SpringBootP03/Beans/Flipkart.java
@@ -2,9 +2,11 @@ package com.nikhilzzz.SpringBootP03.Beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+//@CofigurationProperties(prefix = "db")
 public class Flipkart {
     Flipkart(){
         System.out.println("Flipkart CONSTRUCTOR");
@@ -13,8 +15,13 @@ public class Flipkart {
    // @Qualifier("jp")
     Partners pat;
 
+    @Value("${db.url}")
+    String URL;
+
     public void paymentGateway(String Amount)
     {
+
+        System.out.println(URL);
         pat.payments(Amount);
     }
 }
